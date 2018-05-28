d1353a64699fd4e3a153cdade7808b14ea973333
wangqingxin <wqxin1991@126.com>
2018-05-28 23:55:25 +0800

    DB信息提交

---
+++ player.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.40 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `player` */
+
+DROP TABLE IF EXISTS `player`;
+
+CREATE TABLE `player` (
+  `playerId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `exp` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `passward` varchar(255) DEFAULT NULL,
+  `playerName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`playerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `player` */
+
+insert  into `player`(`playerId`,`exp`,`level`,`passward`,`playerName`) values (1,1,2,'3','xiaoming'),(2,2,9,'3','小红'),(3,0,0,'3','yangyang'),(4,0,0,'123','庆新');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
